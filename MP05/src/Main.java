4d9ebb27c6cdb3b4f414700495a23974d4d05508
Manel <manelfa@elpuig.xeill.net>
2022-01-24 14:48:55 +0100

    Proba commit

--- MP05/src/Main.java
+++ MP05/src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public  static void main(String[] args) {
         System.out.println("Hola Mon, com va tot");
-        System.out.println("Hola Mon, com va");
+        System.out.println("Hola Mon");
 
 
     }
